c300ffe1276a05d3ebbd5052de2c14ff2d9a37ef
Aden Herold <aden.herold1@gmail.com>
2020-07-24 10:42:30 +1000

    Project: Added graphql-codegen + graphql-codegen-typescript dev dependencies to generate schema typescript types
    Chore: Updated query resolvers to use types generated by graphql-codegen

--- src/resolvers/query/index.ts
+++ src/resolvers/query/index.ts
@@ -4,22 +4,31 @@ import {
   getMovieSearchUrl,
   getMovieDetailsUrl,
 } from "../../utils/urls";
+import {
+  SearchResult,
+  QuerySearchArgs,
+  QueryDetailsArgs,
+  FilmDetails,
+} from "../../generated/types";
 
 const Query = {
-  hello: (parent, args, context) => {
+  hello: (parent, args, context): string => {
     return "Hello World";
   },
-  discover: async (parent, args, context) => {
+  discover: async (parent, args): Promise<SearchResult[]> => {
     const url = getMovieDiscoverUrl(1);
     const { results } = await get(url);
     return results;
   },
-  search: async (parent, { query }, context) => {
+  search: async (
+    parent,
+    { query }: QuerySearchArgs
+  ): Promise<SearchResult[]> => {
     const url = getMovieSearchUrl(query);
     const { results } = await get(url);
     return results;
   },
-  details: (parent, { movieId }, context) => {
+  details: (parent, { movieId }: QueryDetailsArgs): Promise<FilmDetails> => {
     const url = getMovieDetailsUrl(movieId);
     return get(url);
   },
